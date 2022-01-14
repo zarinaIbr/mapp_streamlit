d9836d1461fad3445a3b0ba14762da37cb6f8dfa
zarinaIbr <64524369+zarinaIbr@users.noreply.github.com>
2022-01-14 13:51:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -150,7 +150,7 @@ if len(selected_indices) == 1:
 #         b = io.BytesIO()
 #         pickle.dump(st.session_state.good_info, b)
 #         b64 = base64.b64encode(b.getvalue()).decode()
-            if st.download_button(label="Download data", data=csv_good, file_name=f"good_data_{time}.pickle"):
+            if st.download_button(label="Download data", data=csv_good, file_name=f"good_data_{time}.csv"):
                 download_to_excel(st.session_state.good_info, type_aam=True)
             #           download_to_drive(st.session_state.good_info, datetime_NY.strftime("%H_%M"), type_aam=True)
                 del st.session_state.good_info
