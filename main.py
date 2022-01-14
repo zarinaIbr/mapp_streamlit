d63228dd02ea65b9e3b69063a73bf53d404d6527
zarinaIbr <64524369+zarinaIbr@users.noreply.github.com>
2022-01-14 13:47:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -149,10 +149,10 @@ if len(selected_indices) == 1:
 #         b = io.BytesIO()
 #         pickle.dump(st.session_state.good_info, b)
 #         b64 = base64.b64encode(b.getvalue()).decode()
-          if st.download_button(label="Download data", data=csv_good, file_name=f"good_data_{time}.pickle"):
-            download_to_excel(st.session_state.good_info, type_aam=True)
-#           download_to_drive(st.session_state.good_info, datetime_NY.strftime("%H_%M"), type_aam=True)
-            del st.session_state.good_info
+            if st.download_button(label="Download data", data=csv_good, file_name=f"good_data_{time}.pickle"):
+                download_to_excel(st.session_state.good_info, type_aam=True)
+            #           download_to_drive(st.session_state.good_info, datetime_NY.strftime("%H_%M"), type_aam=True)
+                del st.session_state.good_info
     elif checkbox_no and not checkbox_yes:
         if 'before' not in st.session_state and 'after' not in st.session_state:
             st.session_state.before, st.session_state.after = [], []
